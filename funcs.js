5178bfa061fc68ac794fc7f057fc4e94538fe38b
Andrew Garcia <garciaa38@gmail.com>
2023-10-10 21:00:13 -0400

    stuck on isOdd

--- funcs.js
+++ funcs.js
@@ -1,9 +1,26 @@
 function isFive(num) {
   // Your code here
+  if (num === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isOdd(number) {
   // Your code here
+  try {
+    if (typeof number !== typeof(Number)) {
+      throw new Error('Must be a number!')
+    }
+    if (number % 2 !== 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 function myRange(min, max, step = 1) {
